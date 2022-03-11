f12da48827721b7e3f29aff6ac1c66d96beb0b14
Grant Finneman <grant@finneman.org>
2022-03-11 13:59:10 -0600

    I added logging to the classes

--- betterBattleship/betterBattleship.py
+++ betterBattleship/betterBattleship.py
@@ -1,30 +1,32 @@
-import logging, numpy as np
-from collections import namedtuple
+import logging
+import numpy as np
+import sys
+from classes.classes import Board
+
+# ======================================================================================================================
+# The logging setup is kinda new to me
+logger = logging.getLogger('battleship')
+logger.setLevel(logging.DEBUG)
+
+# Filehandler so logs get sent to a file
+fh = logging.FileHandler('battleship.log')
+fh.setLevel(logging.DEBUG)
+
+# Formatter for the log messages
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+fh.setFormatter(formatter)
+
+# Adds handlers to logger
+logger.addHandler(fh)
+# ======================================================================================================================
 
 Version = "0.01"
 
-
-class Board:
-    def __init__(self, player_name, ships_positions, rows=10, columns=10):
-        self.player_name = player_name
-        self.size = namedtuple("boardSize", "rows columns")(rows, columns)
-        self.ship_info = ships_positions
-        self.tiles = np.full(shape=(rows, columns), fill_value="=", dtype=object)
-        return
-
-    def __repr__(self):
-        return f"A board owned by {self.player_name} with a size of {self.size.rows}x{self.size.columns}"
-
-    @property
-    def tiles(self):
-        return self._tiles
-
-    @tiles.setter
-    def tiles(self, new_tiles):
-        self._tiles = new_tiles
-        return
-
-
-grant_board = Board("grant", {(1, 2): "destroyer"})
+grant_board = Board("grant")
 print(grant_board)
-print(grant_board.tiles)
+
+for r in range(grant_board.size.rows):
+    row_string = " ".join(grant_board.tiles[r, :])
+    print(row_string)
+
+
--- betterBattleship/classes/classes.py
+++ betterBattleship/classes/classes.py
@@ -1,25 +1,22 @@
-import logging, numpy as np
+import logging
+import numpy as np
 from collections import namedtuple
+from dataclasses import dataclass
 
-"""
-Main class that defines the boards where ships will be placed
-"""
+module_logger = logging.getLogger('battleship.classes')
+
+
 class Board:
-    def __init__(self, player_name, ships_positions, rows=10, columns=10):
+    """
+    Main class that defines the boards where ships will be placed
+    """
+
+    def __init__(self, player_name, rows=10, columns=10, water_char='~'):
+        self.logger = logging.getLogger('battleship.classes.Board')
         self.player_name = player_name
         self.size = namedtuple("boardSize", "rows columns")(rows, columns)
-        self.ship_info = ships_positions
-        self.tiles = np.full(shape=(rows, columns), fill_value="=", dtype=object)
+        self.tiles = np.full(shape=(rows, columns), fill_value=water_char, dtype=object)
         return
 
     def __repr__(self):
         return f"A board owned by {self.player_name} with a size of {self.size.rows}x{self.size.columns}"
-
-    @property
-    def tiles(self):
-        return self._tiles
-
-    @tiles.setter
-    def tiles(self, new_tiles):
-        self._tiles = new_tiles
-        return
