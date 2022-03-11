0556f2b73d73c8b32b060b3120adbac98a2f4043
Grant Finneman <grant@finneman.org>
2022-03-11 10:56:58 -0600

    Added a general structure document

--- betterBattleship/betterBattleship.py
+++ betterBattleship/betterBattleship.py
@@ -1,6 +1,8 @@
 import logging, numpy as np
 from collections import namedtuple
 
+Version = "0.01"
+
 class Board:
     def __init__(self, player_name, ships_positions, rows=10, columns=10):
         self.player_name = player_name
@@ -8,21 +10,20 @@ class Board:
         self.ship_info = ships_positions
         self.tiles = np.full(shape=(rows, columns), fill_value="=", dtype=object)
         return
-    
+
     def __repr__(self):
         return f"A board owned by {self.player_name} with a size of {self.size.rows}x{self.size.columns}"
-    
+
     @property
     def tiles(self):
         return self._tiles
-    
+
     @tiles.setter
     def tiles(self, new_tiles):
         self._tiles = new_tiles
         return
-    
-    
+
+
 grant_board = Board("grant", {(1,2) : "destroyer"})
 print(grant_board)
-
 print(grant_board.tiles)
